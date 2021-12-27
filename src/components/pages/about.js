870865c994bf6b0fb14e508dfc60d5b503ac851c
choward1793 <choward1793@yahoo.com>
2021-12-27 14:09:26 -0600

    2nd push

---
+++ src/components/pages/about.js
@@ -0,0 +1,20 @@
+import React from "react";
+import profilePicture from "../../../static/assets/images/bio/headshot.jpg";
+
+export default function() {
+    return (
+        <div className="content-page-wrapper">
+            <div className="left-column"
+              style={{
+                background: "url(" + profilePicture + ") no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "center"
+              }}
+            />
+
+            <div className="right-column">
+              Bio information goes here
+            </div>
+        </div>
+    );
+}
